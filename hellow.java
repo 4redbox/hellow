ad1fe4478ba3260832259c3ace5ff15a303a6d74
Saravanesh <raja4redbox@gmail.com>
2024-12-20 18:30:39 +0530

    Update hellow.java

--- hellow.java
+++ hellow.java
@@ -1,6 +1,6 @@
 public class hellow {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 6; i++) {
             System.out.println("Hello Worldsss " + i);
         }
     }
